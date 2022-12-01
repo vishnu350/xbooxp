ff612a3d49fc2096bbc9fe745868ccbf219b5e3b
Vishnu P <5123344+vishnu350@users.noreply.github.com>
2022-12-01 23:28:30 +0800

    Merged changes from jpelletier:     https://sourceforge.net/projects/xbooloader

--- src/io.h
+++ src/io.h
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with xbooxp.  If not, see <http://www.gnu.org/licenses/>.
 */
+#if defined(_WIN32) || defined(_WIN64)
 #include "windows.h"
 
 typedef void (WINAPI *PORTOUT) (short int Port, char Data);
@@ -89,3 +90,177 @@ int LoadIODLL() {
 
 	return 0;
 }
+
+#elif defined(__linux__)
+#include <stdlib.h>
+#include <sys/io.h>
+
+/* ioport(port)         permit access to an I/O port.
+ * port                 I/O address to enable
+ */
+void
+ioport(int port)
+{
+    if (ioperm(port, 1, 1)) {perror("ioperm"); exit(1);}
+}
+
+void PortInit(short int Port)
+{
+  printf ("PortInit: %04X\n",Port & 0xFFFF);
+
+  if ((Port & 0xFFFF) < 0x400)
+  {
+    printf ("using ioperm\n");
+    ioport(Port);
+    ioport(Port+1);
+    ioport(Port+2);
+  }
+  else
+  {
+    printf ("using iopl\n");
+    if (iopl(3)) exit(1); /* needs root priviledges */
+  }
+}
+
+void PortOut(short int Port, char Data)
+{
+  outb(Data, Port);
+}
+
+char PortIn(short int Port)
+{
+  return inb(Port);
+}
+
+#elif defined(__NetBSD__)
+
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <machine/sysarch.h>
+#include <machine/pio.h>
+
+/* ioport(port)         permit access to an I/O port.
+ * port                 I/O address to enable
+ */
+void
+ioport(int port)
+{
+  u_long iomap[32];
+  struct i386_set_ioperm_args ioperm;
+
+  ioperm.iomap = iomap;
+  syscall(SYS_sysarch, I386_GET_IOPERM, (char *) &ioperm);
+  iomap[port >> 5] &= ~(1 << (port & 0x1f));
+  syscall(SYS_sysarch, I386_SET_IOPERM, (char *) &ioperm);
+}
+
+void PortInit(short int Port)
+{
+  ioport(Port);
+  ioport(Port+1);
+  ioport(Port+2);
+}
+
+void PortOut(short int Port, char Data)
+{
+  outb(Port, Data);
+}
+
+char PortIn(short int Port)
+{
+  return inb(Port);
+}
+
+#elif defined(__OpenBSD__)
+
+#include <sys/types.h>
+#include <machine/sysarch.h>
+#include <machine/pio.h>
+
+/*
+ * Modifies the I/O permission bitmap for the current process, so that the
+ * process could read/write the I/O port 'port'.
+ */
+void
+ioport(int port)
+{
+  u_long iomap[32];
+
+  if (i386_get_ioperm(iomap) != 0)
+    fprintf(stderr,"i386_get_permio: %d",errno);
+
+  iomap[((unsigned short) port) / (4 * 8)] &= ~(((u_long) 1) << (((unsigned short) port) % (4 * 8)));
+
+  if (i386_set_ioperm(iomap) != 0)
+    fprintf(stderr,"i386_set_permio: %d",errno);
+}
+
+void PortInit(short int Port)
+{
+  ioport(Port);
+  ioport(Port+1);
+  ioport(Port+2);
+}
+
+void PortOut(short int Port, char Data)
+{
+  outb(Port, Data);
+}
+
+char PortIn(short int Port)
+{
+  return inb(Port);
+}
+
+#elif defined(__FreeBSD__)
+#include <sys/types.h>
+#include <machine/cpufunc.h>
+#include <machine/sysarch.h>
+
+#if (__FreeBSD_version > 500000)
+static FILE * port_access_handle = NULL;
+#endif
+
+void
+ioport(int port)
+{
+/* Get access to a specific port */
+#if (__FreeBSD_version > 500000)
+  if( port_access_handle
+    || (port_access_handle = fopen("/dev/io", "rw")) != NULL )
+  {
+    if (i386_set_ioperm(port, 1, 1) != 0)
+    {
+      fprintf(stderr,"i386_set_permio: %d",errno);
+    }
+  }
+  else
+  {
+    fprintf(stderr,"fopen /dev/io: %d",errno);
+  };
+#else
+  if (i386_set_ioperm(port, 1, 1) != 0)
+  {
+    fprintf(stderr,"i386_set_permio: %d",errno);
+  }
+#endif
+}
+
+void PortInit(short int Port)
+{
+  ioport(Port);
+  ioport(Port+1);
+  ioport(Port+2);
+}
+
+void PortOut(short int Port, char Data)
+{
+  outb(Port, Data);
+}
+
+char PortIn(short int Port)
+{
+  return inb(Port);
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,14 @@
     You should have received a copy of the GNU General Public License
     along with xbooxp.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "stdio.h"
+#include <stdio.h>
+
+#if defined(_WIN32) || defined(_WIN64)
 #include "conio.h"
+#elif defined(__linux__)
+#include <unistd.h>
+#endif
+
 #include "io.h"
 #include "fstream"
 #include "data.h"
@@ -25,11 +31,11 @@
 using namespace std;
 
 unsigned char filesize[2],no_header=0,test_flag=0;			//filesize transfer(1st and 2nd byte), no header flag(to know whether to send dummy header),beta_test flag
-unsigned char rec[4]={0xFF,0xFF,0xFF,0xFF},dont_care=0;		//Received Data from GBA (used by Sendata32 func.),dont care input flag(for sendata func)
+unsigned char rec[4]={0xFF,0xFF,0xFF,0xFF},dont_care=0;		//Received Data from GBA (used by Sendata32 func.),don't care input flag(for sendata func)
 unsigned long filesize_32;									//file transfer function length
 double bit_delay=0,unit_delay=11;							//delay bit transfer value(set here), delay unit transfer value (set here)
-signed short port_address=0x378,filename=0;					//parellel port address,file name flag
-unsigned long enc,CRC,rr,hh,cc,var_30,var_C = 0x0C387;		//Encrypyion and crc variables(all of them important)
+unsigned short port_address=0x378,filename=0;					//parallel port address,file name flag
+unsigned long enc,CRC,rr,hh,cc,var_30,var_C = 0x0C387;		//Encryption and crc variables(all of them important)
 ifstream game;												//gba file pointer, using ifstream function
 
 void Usage(char *name);
@@ -42,23 +48,22 @@ void SendData32(unsigned char a, unsigned char b, unsigned char c, unsigned char
 void Initialize();
 void SendMainData();
 
-void main(unsigned int count,char **arg)
+int main(int count,char **arg)
 {
 	unsigned long x=0,y=0,f;
-	short temp[4];
+	unsigned char c;
+	short temp[5];
 
+#if defined(_WIN32) || defined(_WIN64)
 	printf ("\nxbooxp v1.2, GBA multiboot transfer software designed for Windows XP/2000.\nCopyright 2005 by Vishnu.\n\n");
-	BeginTransfer();
-
-	if (count<2)
-		Usage(arg[0]);
-
-	if (count>6)
+#elif defined(__linux__)
+	printf ("\nxbooxp v1.2, GBA multiboot transfer software for Linux.\nCopyright 2005 by Vishnu.\n\n");
+#endif
+	if (count<2 || count>6)
 	{
-		printf ("Too many arguments.....Read intructions more carefully.\n\n");
-		QuitProgram();
-	}
-
+		Usage(arg[0]);
+		return 1;
+    }
 	else
 	{
 		for (x=1; x<=(count-1) ;x++)							//Argument Check loop
@@ -79,25 +84,29 @@ void main(unsigned int count,char **arg)
 					else
 					{
 						printf ("Invalid argument %s, if delay=1 then type -d1.\n\n",arg[x]);
-						QuitProgram();
+						return 1;
 					}
 				}
 
 				else if (arg[x][1]=='p')						//checking for -p
 				{
-					for (y=0; y<3; y++)
+					/* JP: allow for 4 hexa digits for use with PCMCIA and PCI/PCI express parallel ports */
+					for (y=0; y<4; y++)
 					{
-						if ((arg[x][y+2]>='0')&&(arg[x][y+2]<='9'))
-							temp[y]=arg[x][y+2]-48;
-						else if ((arg[x][y+2]>='A')&&(arg[x][y+2]<='F'))
-							temp[y]=arg[x][y+2]-55;
+                        c = arg[x][y+2];
+						if ((c>='0')&&(c<='9'))
+							temp[y]=c-0x30;
+						else if ((c>='A')&&(c<='F'))
+							temp[y]=c-0x41+0x0A;
+						else if ((c>='a')&&(c<='f'))
+							temp[y]=c-0x61+0x0A;
 						else
 						{
 							printf ("Invalid port address %s\nIf your port address is 0x278, then type -p278.\n\n",arg[x]);
-							QuitProgram();
+							return 1;
 						}
 					}
-					port_address=(temp[0]<<8)+(temp[1]<<4)+temp[2];
+					port_address=(temp[0]<<12)+(temp[1]<<8)+(temp[2]<<4)+temp[3];
 				}
 
 				else if (arg[x][1]=='t')						//checking for -t
@@ -106,7 +115,7 @@ void main(unsigned int count,char **arg)
 				else
 				{
 					printf ("Invalid argument %s\n\n",arg[x]);
-					QuitProgram();
+					return 1;
 				}
 			}
 
@@ -115,18 +124,18 @@ void main(unsigned int count,char **arg)
 				f=x;
 				filename=1;
 			}
-			
+
 			else												//Else is garbage
 			{
 				printf ("Invalid command %s\n\n",arg[x]);
-				QuitProgram();
+				return 1;
 			}
 		}
 
 		if (filename==0)										//If no file name
 		{
 			printf ("No file specified.....\n\n");
-			QuitProgram();
+			return 1;
 		}
 
 		else if (filename==1)									//If file name exists, then...
@@ -135,16 +144,21 @@ void main(unsigned int count,char **arg)
 			if (game.fail())
 			{
 				printf ("Error opening file %s\n\n",arg[f]);
-				QuitProgram();
+				return 1;
 			}
 		}
 	}
+    /* Only begin doing something after filtering out option and file errors */
+    BeginTransfer();
 
 	//printf ("Header=%d, bit delay=%.3f, unit delay=%.3f, Port=%X, File=%s\n\n",no_header,bit_delay,unit_delay,port_address,arg[f]);
 	FileCalc();
 	Initialize();
 	SendMainData();
+#if defined(_WIN32) || defined(_WIN64)
 	UnloadIODLL();
+#endif
+    return 0;
 }
 
 void Usage(char *name)
@@ -156,33 +170,36 @@ void Usage(char *name)
 	printf ("                       standard header file.\n\n");
 	printf ("   -d[value]    Put value here to increase delay, any value from 1 to 9,\n");
 	printf ("                eg. -d3. Use this only if you are having transfer problems.\n\n");
-	printf ("   -p[port]     Input your parellel port address, eg -p278. If no value is\n");
+	printf ("   -p[port]     Input your parallel port address, eg -p278. If no value is\n");
 	printf ("                specified then the default port will be 0x378. Use CAPITAL\n");
 	printf ("                letters for port addresses with alphabetical values,eg. -p3BC.\n\n");
 	printf ("   -t           For testing whether a program can be sent without actually\n");
 	printf ("                starting the program. This allows you to send another program\n");
-	printf ("                without reseting the GBA. For testing purposes only.\n\n");
+	printf ("                without resetting the GBA. For testing purposes only.\n\n");
 	QuitProgram();
 }
 inline void BeginTransfer()
 {
+#if defined(_WIN32) || defined(_WIN64)
 	int x;
 
 	x=LoadIODLL();
 	if (x==1)
 	{
-		printf ("Error : io.dll is missing.\n\n");
+		fprintf(stderr,"Error : io.dll is missing.\n\n");
 		QuitProgram();
 	}
 
-	
 	x=IsDriverInstalled();
 	if (x==0)
 	{
-		printf ("io.dll has just been initialized. Please restart the program. This happens the\nfirst time the program is used.\n\n");
+		fprintf(stderr,"io.dll has just been initialized. Please restart the program. This happens the\nfirst time the program is used.\n\n");
 		QuitProgram();
 	}
-
+#else
+    fprintf(stderr,"Port address: %04X\n",port_address);
+    PortInit(port_address);
+#endif
 	PortOut(port_address+2,0x04);						//Idle begin, sets S0 high and SC high
 }
 
@@ -198,8 +215,12 @@ void DelayLoop(double delay_count,char delay_type)		//delay function(timing purp
 			byte_low=PortIn(port_address);
 	}
 
-	else if ((delay_type==1)&&(delay_count!=0))			//delay function using pc speaker timer, very accurate and stable, from 0.001ms to 54ms.
+#if defined(_WIN32) || defined(_WIN64)
+#elif defined(__linux__)
+#else
+	else if ((delay_type==1)&&(delay_count!=0))
 	{
+	//delay function using pc speaker timer, very accurate and stable, from 0.001ms to 54ms.
 		required_count = delay_count/(0.0008380958);
 		byte_low=0x0B4;
 		PortOut(0x043,byte_low);
@@ -220,13 +241,16 @@ void DelayLoop(double delay_count,char delay_type)		//delay function(timing purp
 		byte_low=0;
 		PortOut(0x061,byte_low);
 	}
+#endif
 }
 
 void QuitProgram()
 {
-	printf ("Press any key to cotinue...");
-	getch();
+	//printf ("Press any key to continue...");
+	//getch();
+#if defined(_WIN32) || defined(_WIN64)
 	UnloadIODLL();
+#endif
 	exit(0);
 }
 
@@ -273,7 +297,7 @@ char SendData(unsigned char x)							//Send char to GBA serial style 1/16/1
 
 	for(loop=7; loop>=0; loop--)
 	{
-		temp1=(~(x>>loop))&(0x01);					//1 or 0,then invert(damn inverted parellel port ctr reg)
+		temp1=(~(x>>loop))&(0x01);					//1 or 0,then invert(damn inverted parallel port ctr reg)
 		sendbit=send|(temp1<<1);
 
 		PortOut((port_address+2),sendbit);			//send bit control port
@@ -282,8 +306,8 @@ char SendData(unsigned char x)							//Send char to GBA serial style 1/16/1
 		temp1=sendbit&(0xFA);						//set clock high to read
 		PortOut((port_address+2),temp1);			//then send
 		DelayLoop(bit_delay,0);						//wait again
-		
-		if (dont_care==1)							//if dont care=1, then no need to read (faster transfer this way)
+
+		if (dont_care==1)							//if don't care=1, then no need to read (faster transfer this way)
 			temp2=0;
 
 		else										//else, read read status port
@@ -292,7 +316,7 @@ char SendData(unsigned char x)							//Send char to GBA serial style 1/16/1
 			temp2=temp2|(((temp1>>6)&(0x01))<<(loop));
 		}
 	}
-	
+
 	return (temp2);
 }
 
@@ -316,17 +340,22 @@ START:
 	{
 		if (loop>7)
 		{
-			DelayLoop(10,1);														//If failed wait 65ms
+#if defined(_WIN32) || defined(_WIN64)
+#elif defined(__linux__)
+			usleep(65000);
+#else
+			DelayLoop(10,1);	//If failed wait 65ms
+#endif
 			loop=0;
 			timeout++;
 		}
-		
+
 		if (timeout>240)
 		{
 			printf("Timeout : Failed\n\nCheck if your GBA is on. Hold START + SELECT during the boot up process.\nElse, there is a problem with your cable.\n\n");
 			QuitProgram();
 		}
-		
+
 		SendData32(0,0,0x62,0x02);
 	}
 
@@ -335,8 +364,13 @@ START:
 		SendData32(0,0,0x62,0x02);
 		if (rec[0]!=0x72)								//Try again if failed, wait 62.5ms
 		{
+#if defined(_WIN32) || defined(_WIN64)
+#elif defined(__linux__)
+			usleep(62500);
+#else
 			DelayLoop(50,1);
 			DelayLoop(12.5,1);
+#endif
 			goto START;
 		}
 	}
@@ -367,19 +401,25 @@ START:
 	while (!(rec[0]==0x73))								//Wait until slave reply 73cc
 		SendData32(0,0,0x63,0xF7);
 
-	cc=rec[1];											//get the encyrption
+	cc=rec[1];											//get the encryption
 	hh = ((cc&0x0FF)+0x20F)&0x0FF;						//
 	enc = ((cc&0x0FF)<<8)|(0x0FFFF00F7);				//
 
 	SendData32(0,0,0x64,hh);							//Handshake
 
+#if defined(_WIN32) || defined(_WIN64)
+#elif defined(__linux__)
+	usleep(62500);
+#else
 	DelayLoop(50,1);									//Pause 1/16 sec
 	DelayLoop(12.5,1);
+#endif
 }
 
 void SendMainData()
 {
-	unsigned long data32, pos=0xC0, loop, bit, timeout=0, percent=0;
+	unsigned long data32, pos=0xC0, loop, timeout=0;
+	int bit, percent=0;
 
 	SendData32(0,0,filesize[0],filesize[1]);										//transfer length
 	rr = rec[1];
@@ -389,7 +429,7 @@ void SendMainData()
 	dont_care=1;
 	for (loop=0; loop<filesize_32; loop++)											//Send Program
 	{
-		data32 = program[pos] + (program[pos+1] << 8) 
+		data32 = program[pos] + (program[pos+1] << 8)
 				+ (program[pos+2]<<0x10) + (program[pos+3] <<0x18);
 
 		CRC = data32;																//CRC calculation
@@ -398,7 +438,7 @@ void SendMainData()
 			var_30 = var_C ^ CRC;
 			var_C = var_C >> 1;
 			CRC = CRC >> 1;
-			if (var_30&0x01) 
+			if (var_30&0x01)
 				var_C = var_C^0x0C37B;
 		}
 
@@ -434,7 +474,7 @@ void SendMainData()
 		var_30 = var_C ^ CRC;
 		var_C  = var_C>>1;
 		CRC = CRC>>1;
-		if (var_30 & 0x01) 
+		if (var_30 & 0x01)
 			var_C = var_C ^ 0x0C37B;
 	}
 
